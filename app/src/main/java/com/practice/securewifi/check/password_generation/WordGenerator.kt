5e08d0290f16a72c2585c92ea8de7de1745ff937
Kirill Mokhnatkin <librarian4832@gmail.com>
2024-04-14 01:40:41 +0300

    Adjusted generation with years

--- app/src/main/java/com/practice/securewifi/check/password_generation/WordGenerator.kt
+++ app/src/main/java/com/practice/securewifi/check/password_generation/WordGenerator.kt
@@ -6,17 +6,6 @@ interface WordGenerator {
 
     fun generateFromWordAndPopularNumbers(word: String): List<String> {
         val generatedPasswords: MutableList<String> = mutableListOf()
-
-        val years = listOf(
-            "2000", "2001", "2002", "2003", "2004", "2005", "2006", "2007",
-            "2008", "2009", "2010", "2011", "2012", "2013", "2014", "2015",
-            "2016", "2017", "2018", "2019", "2020", "2021", "2022", "2023",
-            "2024"
-        )
-        years.forEach {
-            generatedPasswords.add(word + it)
-        }
-
         val wordLength = word.length
         if (wordLength < 8) {
             generatedPasswords.add(word + generateWordFromRepeatingDigit('1', 8 - wordLength))
@@ -27,6 +16,16 @@ interface WordGenerator {
         if (wordLength >= 6) {
             generatedPasswords.add(word + "69")
             generatedPasswords.add(word + "228")
+        }
+        val years = listOf(
+            "2024", "2023", "2022", "2021", "2020", "2019",
+            "2018", "2017", "2016", "2015", "2014", "2013",
+            "2012", "2011", "2010", "2009", "2008", "2007",
+            "2006", "2005", "2004", "2003", "2002", "2001",
+            "2000"
+        )
+        years.forEach {
+            generatedPasswords.add(word + it)
         }
         return generatedPasswords
     }
